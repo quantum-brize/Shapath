2f768fbe41fb472253cc978a408473e743288263
rohan0420 <skrohan0420@gmail.com>
2024-02-16 19:15:57 +0530

    payment gateway partly done

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -339,5 +339,41 @@ class Admin extends Common
 
     }
 
+    public function proccess_payment()
+    {
+        $this->init_model(MODEL_PAGES);
+
+
+        $name = $this->input->post('name');
+        $email = $this->input->post('email');
+        $phone = $this->input->post('phone');
+        $pan = $this->input->post('pan');
+        $amount = $this->input->post('amount');
+        $project_id = $this->input->post('project_id');
+        $address = $this->input->post('address');
+        $pin = $this->input->post('pin');
+
+        $insert_data = [
+            "uid" => $this->generate_uid(UID_DONATION),
+            "name" => $name,
+            "email" => $email,
+            "phone" => $phone,
+            "pan" => $pan,
+            "address" => $address,
+            "amount" => $amount,
+            "pin" => $pin,
+            "project_id" => $project_id,
+        ];
+
+        $add_new_donation = $this->Pages_model->add_new_donation($insert_data);
+
+        if ($add_new_donation) {
+            echo json_encode(array("status"=> true));
+
+        } else {
+            echo json_encode(array("status"=> false));
+        }
+    }
+
 
 }
